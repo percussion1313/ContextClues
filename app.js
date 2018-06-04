d2f22ffacdc26cd5625370d10bdb2c35fa2d14af
percussion1313 <peterjacobgrant@gmail.com>
2018-06-04 17:05:39 -0500

    Cleaned up code. For some reason it keeps looping the alert box

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-function makeFunc() {
-    var accusation = ''
-    return function() {
-        var i = 1
-        while (i<=100) {
-            accusation += `<h3>Accusation ${i}</h3>`;
-            i++;  
-        }
-        $('body').append(accusation);
-    }
-    return accusation;
-}
+$(document).ready(function() {
+        var friends = ['Dee', 'Dennis', 'Charlie', 'Frank', 'Pondy'];
+        var locations = ['Pub', 'Aparment', 'School', 'Hospital', 'Bathroom', 'Closet', 'Ditch', 'Hospital', 'Top Golf', 'News Room'];
+        var weapons = ['Cheesegrater', 'Hockey Puck', 'Stabby Thing', 'Coffee Mug', 'Beer Bottle', 'Kitten Mittons', 'Rubber Glove', 'Sticky Thing', 'Toothbrush', 'iPhone X', 'Popcorn Bucket', 'Door Handle', 'Electic Plug', 'Furnace', 'Plush Animal', 'Mirror', 'Comforter', 'Duster', 'Spoon', 'Piece of Construction Paper'];
 
-var myFunc = makeFunc();
-myFunc();
+        for (var i=1; i <= 100; i++) {
+            $('body').append(`<h3>Accusation ${i}</h3>`);
+            $('h3').click(accuseSomeone(i, friends, locations, weapons));
+            }
+        
+        function accuseSomeone (i, friends, locations, weapons) {
+            return function () {
+                alert(`Accusation: ${i} I accuse ${friends[i%5]} with the ${weapons[i%10]} in the ${locations[i%5]}`);
+        }
+    }
+})
+
+
