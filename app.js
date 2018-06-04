9cc69727416e17009741866c344eb9aee17305c7
percussion1313 <peterjacobgrant@gmail.com>
2018-06-04 15:31:25 -0500

    made the closure functional

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-function addAccusation() {
+function makeFunc() {
     var accusation = ''
-    var i = 1
-    while (i<=100) {
-        accusation += `<h3>Accusation ${i}</h3>`;
-        i++;  
+    return function() {
+        var i = 1
+        while (i<=100) {
+            accusation += `<h3>Accusation ${i}</h3>`;
+            i++;  
+        }
+        $('body').append(accusation);
     }
-    $('body').append(accusation);
+    return accusation;
 }
 
-addAccusation()
+var myFunc = makeFunc();
+myFunc();
